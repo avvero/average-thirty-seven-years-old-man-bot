a0df3b5efba1c15fadfefc31ec4de1adfab3c2c2
belyaev-ay <anton.belyaev@sibit.tech>
2022-08-14 01:41:02 +0700

    fix bug

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func main() {
 			webhookRequest.Message.Chat.Title + ": " + webhookRequest.Message.Text)
 
 		if !Contains([]string{"-1001733786877", "245851441", "-578279468"}, strconv.FormatInt(webhookRequest.Message.Chat.Id, 10)) {
-			sendMessage(webhookRequest.Message.Chat.Id, "Не пиши мне вообще, понял?")
+			sendMessage(webhookRequest.Message.Chat.Id, "Хули нада, пес?")
 			return
 		}
 		if strings.EqualFold(webhookRequest.Message.Text, "gg") {
