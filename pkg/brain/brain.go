a69fc5ed7d9c5fdb0a28fda10c919a765f83755b
Anton Belyaev <avvero@mail.ru>
2022-11-28 20:21:59 +0700

    Update brain.go

--- pkg/brain/brain.go
+++ pkg/brain/brain.go
@@ -49,6 +49,7 @@ func (brain *Brain) Decision(chatId int64, text string) (respond bool, response 
 		when(is(brain.randomFactor), random(300), length(14)).then(NewKhaleesifyIntention()).
 		when(is(brain.randomFactor), random(500)).then(&ConfuciusPhrasesIntention{}).
 		when(is(brain.randomFactor), random(100), contains("опять")).say("не опять, а снова").
+		when(contains("мог быть", "могли быть", "могла быть", "могло быть")).say("словами либерашки").
 		//when(is(brain.randomFactor), random(100), contains("купил")).say("А не пиздишь? Аренда это не покупка").
 		when(its("gg")).say("gg").
 		when(its("нет")).then(&NoThenPhraseIntention{}).
