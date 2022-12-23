543b8d32e8e85895011fa84383b94a35d43f2ed5
belyaev-ay <anton.belyaev@sibit.tech>
2022-12-23 18:36:21 +0700

    fix

--- pkg/brain/whitelist.go
+++ pkg/brain/whitelist.go
@@ -10,7 +10,7 @@ type Whitelist struct {
 
 func (this Whitelist) Check(chatId int64, text string) (forbidden bool, response string) {
 	if !utils.Contains([]string{"0", "-1001733786877", "245851441", "-578279468", "-831690505"}, strconv.FormatInt(chatId, 10)) {
-		return true, "Mr Moony presents his compliments to Professor Snape, and begs him to keep his " +
+		return true, strconv.FormatInt(chatId, 10) + ": Mr Moony presents his compliments to Professor Snape, and begs him to keep his " +
 			"abnormally large nose out of other people’s business."
 	} else {
 		return false, ""
