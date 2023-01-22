e5133c56a82d3edea862969cd684170ee2772c05
belyaev-ay <anton.belyaev@sibit.tech>
2023-01-22 19:55:31 +0700

    fix

--- internal/openai/api.go
+++ internal/openai/api.go
@@ -44,10 +44,10 @@ func NewApiClient(url string, accessKey string) OpenAiClient {
 }
 
 func (apiClient OpenAiClient) Completion(text string) (error, string) {
-	fmt.Printf("Request to: %s\n", apiClient.url)
-	client := http.Client{Timeout: 500 * time.Second}
-
 	text = strings.ReplaceAll(text, "\"", "\\\"")
+
+	fmt.Printf("Request to: %s\n with text: %s\n", apiClient.url, text)
+	client := http.Client{Timeout: 500 * time.Second}
 
 	requestBody, marshalError := json.Marshal(Request{Model: "text-davinci-003", Prompt: text, Temperature: 0.9,
 		MaxTokens: 4097 - len(text), TopP: 1, FrequencyPenalty: 0.0, PresencePenalty: 0.6})
